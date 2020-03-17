4cd3053a6ae90d49931d340967bc3dcecff7f912
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-17 16:52:28 +0100

    bonus ok

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@ $(document).ready(function(){
     var source = $("#cards-template").html();
     var templateCard = Handlebars.compile(source);
 
+    $('.selettore-genere').change(function(){
+        var genereSelected = $(this).val();
+        console.log(genereSelected);
+        if(genereSelected == '') {
+            $('.card').show();
+        } else {
+            $('.card').each(function(){
+                if(genereSelected.toLowerCase() == $(this).data('genere').toLowerCase()) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        }
+    });
+
+
     $.ajax({
         url: "https://flynn.boolean.careers/exercises/api/array/music",
         method: "GET",
@@ -16,18 +33,16 @@ $(document).ready(function(){
                     albumCover: album.poster,
                     titoloDisco: album.title,
                     autore: album.author,
-                    anno: album.year
+                    anno: album.year,
+                    genere: album.genre
                 };
                 var cardAlbum = templateCard(albumTemplate);
                 $('.container-cards').append(cardAlbum);
             }
-
-
         },
         error: function(){
-
+            alert('ERROOOOREEEEEE!!!')
         }
-
     });
 
 
