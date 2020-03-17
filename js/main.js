0651613836ea8b1eb00d16485f7bd6034ac152a3
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-17 16:11:26 +0100

    esercizio ok

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
+$(document).ready(function(){
 
+    var source = $("#cards-template").html();
+    var templateCard = Handlebars.compile(source);
+
+    $.ajax({
+        url: "https://flynn.boolean.careers/exercises/api/array/music",
+        method: "GET",
+        success: function (data) {
+            var albums = data.response;
+            console.log(albums);
+            for (var i = 0; i < albums.length; i++) {
+                var album = albums[i];
+                console.log(album);
+                var albumTemplate = {
+                    albumCover: album.poster,
+                    titoloDisco: album.title,
+                    autore: album.author,
+                    anno: album.year
+                };
+                var cardAlbum = templateCard(albumTemplate);
+                $('.container-cards').append(cardAlbum);
+            }
+
+
+        },
+        error: function(){
+
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
